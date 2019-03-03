29b95b4d312985196340dfe3843caf2a004aef0f
Nam Tran <tranngocnam97@gmail.com>
2019-03-03 02:01:02 -0500

    Changes signal based on instructor GUI inputs

--- gui-client/gui.py
+++ gui-client/gui.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import Button
 from tkinter import Entry
+from tkinter import Frame
+from tkinter import Label
+from tkinter import Radiobutton
 from tkinter import StringVar
 
 from client import Client
@@ -12,20 +15,91 @@ master = tk.Tk("Phantom - Client")
 message = StringVar(master)
 host = StringVar(master)
 
+hr = StringVar(master, value=0)
+threshold = StringVar(master, value=0)
+
+position = StringVar(master)
+
 def connect():
     client.start()
 
-def send_message():
+def send_command():
     client.send_data(message.get())
 
-btn_connect = Button(master, text="Connect", command=connect)
-btn_connect.pack()
+def send_customisations():
+    client.send_data("update")
+    client.send_data("{},{}".format(hr.get(), threshold.get()))
 
-entry_message = Entry(master, textvariable=message)
-entry_message.pack()
+def send_position():
+    client.send_data("position")
+    client.send_data(position.get())
 
-btn_send = Button(master, text="Send", command=send_message)
-btn_send.pack()
+# ============ Connection Space ===============
+frame_connection = Frame(master)
+frame_connection.pack(pady=5)
+
+Label(frame_connection, text="Hostname").pack(side=tk.LEFT)
+
+entry_hostname = Entry(frame_connection, textvariable=host)
+entry_hostname.pack(side=tk.LEFT)
+
+btn_connect = Button(frame_connection, text="Connect", command=connect)
+btn_connect.pack(side=tk.LEFT)
+
+# ============ Customisation Space ===============
+frame_signal = Frame(master)
+frame_signal.pack(pady=5)
+
+Label(frame_signal, text="Heart Rate").grid(row=0, column=0)
+
+entry_hr = Entry(frame_signal, textvariable=hr)
+entry_hr.grid(row=0, column=1)
+
+Label(frame_signal, text="Pacing Threshold").grid(row=1, column=0)
+
+entry_threshold = Entry(frame_signal, textvariable=threshold)
+entry_threshold.grid(row=1, column=1)
+
+btn_send_customisations = Button(frame_signal, text="Send", command=send_customisations)
+btn_send_customisations.grid(row=2, columnspan=2)
+
+# ============ Command Space ===============
+frame_command = Frame(master)
+frame_command.pack(pady=5)
+
+Label(frame_command, text="Command").pack(side=tk.LEFT)
+
+entry_command = Entry(frame_command, textvariable=message)
+entry_command.pack(side=tk.LEFT)
+
+btn_send_command = Button(frame_command, text="Send", command=send_command)
+btn_send_command.pack(side=tk.LEFT)
+
+# ============ Position Selection ===============
+frame_position = Frame(master)
+frame_position.pack(pady=5)
+
+POSITIONS = [
+    ("Superior Vena Cava", "SVC"),
+    ("High Right Atrium", "HRA"),
+    ("Mid Right Atrium", "MRA"),
+    ("Low Right Atrium", "LRA"),
+    ("Inferior Vena Cava", "IVC"),
+    ("Right Ventricle", "RV"),
+    ("Right Ventricular Wall", "RVW"),
+    ("Pulmonary Artery", "PA"),
+    ("Asystole", "RIP"),
+]
+
+position.set("SVC")
+
+Label(frame_position, text="Heart Positions").pack()
+
+for button_text, position_value in POSITIONS:
+    Radiobutton(frame_position, text=button_text, value=position_value, variable=position).pack()
+
+btn_send_position = Button(frame_position, text="Send", command=send_position)
+btn_send_position.pack()
 
 tk.mainloop()
 
--- gui-server/ballooncatheterization.py
+++ gui-server/ballooncatheterization.py
@@ -1,5 +1,7 @@
 # Pip Modules
 import tkinter as Tk
+from tkinter import BooleanVar
+from tkinter import IntVar
 from tkinter import StringVar
 from tkinter import OptionMenu
 
@@ -8,21 +10,27 @@ from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-import multiprocessing
-
-import sys
 import serial
-#import time
 from serial import SerialException
 import serial.tools.list_ports
 
 # Project Modules
 import signals
-import server
+from server import Server
 
-# Socket Connection
-out_queue = multiprocessing.Queue()
-in_queue = multiprocessing.Queue()
+from queue import Queue
+
+socket_queue = Queue()
+
+server = Server(port=911)
+server.start(socket_queue)
+
+root = Tk.Tk()
+
+hr = IntVar(root, value=80)
+threshold = IntVar(root, value=20)
+
+position = StringVar(root, value='RIP')
 
 # Take care of plotting
 fig = plt.Figure(figsize=(14, 4.5), dpi=100)
@@ -30,25 +38,36 @@ fig = plt.Figure(figsize=(14, 4.5), dpi=100)
 new_x = []
 new_y = []
 
-global last_x
 last_x = 0
-
-global last_x_lim
 last_x_lim = 0
-
-global variable
 
 def animate(i):
     # Switch statement for the serial location in order to get which one to do
-    global variable
-    if variable.get() == '':
-        [x, y] = signals.Default_Line()
-    else:
-        [x, y] = signals.High_RA_V1(80)
-
+    global hr
+    global threshold
+    global position
     global last_x
     global last_x_lim
-    
+   
+    if position.get() == 'SVC':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'HRA':
+        [x, y] = signals.High_RA_V1(hr.get())
+    elif position.get() == 'MRA':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'LRA':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'IVC':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'RV':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'RVW':
+        [x, y] = signals.Default_Line()
+    elif position.get() == 'PA':
+        [x, y] = signals.Default_Line()
+    else:
+        [x, y] = signals.Default_Line()
+
     x_val = last_x + x[i]
     
     new_x.append(x_val)
@@ -63,7 +82,6 @@ def animate(i):
         last_x_lim += 5
         ax.set_xlim(last_x_lim, last_x_lim + 5)
     
-
     return line,
 
 def change_dropdown(*args):
@@ -73,7 +91,34 @@ Options=['']
 Options.extend(serial.tools.list_ports.comports())
 
 # GUI Utilisation
-root = Tk.Tk()
+wait_for_update = BooleanVar(root, value=False)
+wait_for_position = BooleanVar(root, value=False)
+
+def read_socket():
+    if not socket_queue.empty():
+        message = socket_queue.get()
+
+        print(message)
+
+        if wait_for_update.get():
+            result = [x.strip() for x in message.decode('utf-8').split(',')]
+
+            hr.set(result[0])
+            threshold.set(result[1])
+
+            wait_for_update.set(False)
+        elif wait_for_position.get():
+            position.set(message.decode('utf-8'))
+            wait_for_position.set(False)
+        else:
+            if message == b'update':
+                wait_for_update.set(True)
+            elif message == b'position':
+                wait_for_position.set(True)
+            elif message == b'close':
+                root.destroy()
+        
+    root.after(10, read_socket)
 
 Tk.Label(root,text="Simulation ECG").pack()
 
@@ -88,7 +133,6 @@ w.pack()
 
 variable.trace('w', change_dropdown)
 
-
 ax = fig.add_subplot(111)
 ax.set_xlim(last_x_lim, 5)
 ax.set_ylim(-5, 5)
@@ -101,4 +145,8 @@ line, = ax.plot(0, 0)
 ax.get_lines()[0].set_color("xkcd:lime")
 ani = animation.FuncAnimation(fig, animate, frames=30, interval=24, repeat=True, blit=True)
 
+root.after(10, read_socket)
+
 Tk.mainloop()
+
+server.stop()
