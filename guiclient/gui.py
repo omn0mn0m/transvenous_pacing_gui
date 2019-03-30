ceb890c2409536d82c40efd9e7ff8b03d98a8ecd
Cooper <cooperpearson15@gmail.com>
2019-03-30 16:52:49 -0400

    adding colors
    hehe

--- guiclient/gui.py
+++ guiclient/gui.py
@@ -45,41 +45,41 @@ def stop_position():
 frame_connection = Frame(master)
 frame_connection.pack(pady=5)
 
-Label(frame_connection, text="Hostname").pack(side=tk.LEFT)
+Label(frame_connection, text="Hostname", font='Times').pack(side=tk.LEFT)
 
-entry_hostname = Entry(frame_connection, textvariable=host)
+entry_hostname = Entry(frame_connection, textvariable=host, font='Times')
 entry_hostname.pack(side=tk.LEFT)
 
-btn_connect = Button(frame_connection, text="Connect", command=connect)
+btn_connect = Button(frame_connection, text="Connect", font='Times', command=connect, fg="green")
 btn_connect.pack(side=tk.LEFT)
 
 # ============ Customisation Space ===============
 frame_signal = Frame(master)
 frame_signal.pack(pady=5)
 
-Label(frame_signal, text="Heart Rate").grid(row=0, column=0)
+Label(frame_signal, text="Heart Rate", font='Times 12 bold').grid(row=0, column=0)
 
 entry_hr = Entry(frame_signal, textvariable=hr)
 entry_hr.grid(row=0, column=1)
 
-Label(frame_signal, text="Pacing Threshold").grid(row=1, column=0)
+Label(frame_signal, text="Pacing Threshold", font='Times 12 bold').grid(row=1, column=0)
 
 entry_threshold = Entry(frame_signal, textvariable=threshold)
 entry_threshold.grid(row=1, column=1)
 
-btn_send_customisations = Button(frame_signal, text="Send", command=send_customisations)
+btn_send_customisations = Button(frame_signal, text="Send", font='Times', command=send_customisations, fg="green")
 btn_send_customisations.grid(row=2, columnspan=2)
 
 # ============ Command Space ===============
 frame_command = Frame(master)
 frame_command.pack(pady=5)
 
-Label(frame_command, text="Command").pack(side=tk.LEFT)
+Label(frame_command, text="Command", font='Times').pack(side=tk.LEFT)
 
 entry_command = Entry(frame_command, textvariable=message)
 entry_command.pack(side=tk.LEFT)
 
-btn_send_command = Button(frame_command, text="Send", command=send_command)
+btn_send_command = Button(frame_command, text="Send", font='Times', command=send_command, fg="green")
 btn_send_command.pack(side=tk.LEFT)
 
 # ============ Position Selection ===============
@@ -98,15 +98,15 @@ POSITIONS = [
     ("Asystole", "RIP"),
 ]
 
-Label(frame_position, text="Heart Positions").pack()
+Label(frame_position, text="Heart Positions", font='Times 16 underline').pack()
 
 for button_text, position_value in POSITIONS:
-    Radiobutton(frame_position, text=button_text, value=position_value, variable=position).pack()
+    Radiobutton(frame_position, text=button_text, font='Times 12 bold', value=position_value, variable=position).pack()
 
-btn_send_position = Button(frame_position, text="Send", command=send_position)
+btn_send_position = Button(frame_position, text="Send", font='Times', command=send_position, fg="green")
 btn_send_position.pack()
 
-btn_stop_position = Button(frame_position, text="Stop", command=stop_position)
+btn_stop_position = Button(frame_position, text="Stop", font='Times', command=stop_position, fg="red")
 btn_stop_position.pack()
 
 # ========== Pathway Selection ==============
@@ -134,15 +134,15 @@ def callback_pathway_2(*args):
 pathway_1.trace('w', callback_pathway_1)
 pathway_2.trace('w', callback_pathway_2)
 
-Label(frame_pathway, text="Pathway Selection 1").pack(pady=5)
+Label(frame_pathway, text="Pathway Selection 1", font='Times 16 underline').pack(pady=5)
 
 for button_text, pathway_value in PATHWAYS_1:
-    Radiobutton(frame_pathway, text=button_text, value=pathway_value, variable=pathway_1).pack()
+    Radiobutton(frame_pathway, text=button_text, font='Times 12 bold', value=pathway_value, variable=pathway_1).pack()
 
-Label(frame_pathway, text="Pathway Selection 1").pack(pady=5)
+Label(frame_pathway, text="Pathway Selection 2", font='Times 16 underline').pack(pady=5)
 
 for button_text, pathway_value in PATHWAYS_2:
-    Radiobutton(frame_pathway, text=button_text, value=pathway_value, variable=pathway_2).pack()
+    Radiobutton(frame_pathway, text=button_text, font='Times 12 bold', value=pathway_value, variable=pathway_2).pack()
 
 tk.mainloop()
 
