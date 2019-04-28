b8f02b8a87b3e76121f73a94ed87afec28321b49
breecathey <36305960+breecathey@users.noreply.github.com>
2019-04-27 21:59:48 -0500

    Update test_signals.py

--- tests/test_signals.py
+++ tests/test_signals.py
@@ -94,15 +94,10 @@ def is_close_enough(list_1, list_2, tolerance=1e-4):
 
     return similar
 
-def test_SVC_V1():
+def test_SVC_V1_20():
     [x,y]=signals.get_signal('SVC', 20, 0)
     assert is_close_enough(x, test_x_svc_20)
     assert is_close_enough(y, test_y_svc_20)
-
-def test_SVC_V1_20():
-    [x,y]=signals.get_signal('SVC', 80, 0)
-    assert is_close_enough(x, test_x_svc)
-    assert is_close_enough(y, test_y_svc)
     
 def test_SVC_V2():
     [x,y] = signals.get_signal('SVC', 80, 1)
