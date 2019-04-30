8c8afdb66d735436328e06741e7cfda49d433fc8
Nam Tran <tranngocnam97@gmail.com>
2019-04-30 00:30:50 -0400

    Fixed ECG not displaying slow times

--- setup.py
+++ setup.py
@@ -2,21 +2,20 @@
 
 from setuptools import setup, find_packages
 
-
-with open('README') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='ballooncatheterization',
-    version='0.1.0',
-    description='Training GUI written in Python 3',
+    name='transvenous_pacing_gui',
+    version='0.7.0',
+    description='Transvenous pacing procedure simulation GUI written in Python 3',
     long_description=readme,
     author='Nam Tran, Cooper Pearson, Richie Beck, Marcel Isper, Brianna Cathey',
     author_email='tranngocnam97@gmail.com',
-    url='https://github.com/omn0mn0m/Balloon-Catheterization-GUI',
+    url='https://github.com/omn0mn0m/transvenous_pacing_gui',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     tests_require=['pytest'],
