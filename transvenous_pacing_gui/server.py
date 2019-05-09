0068e732e1ab82a33e69de192804e7fabad8a9e2
Nam Tran <tranngocnam97@gmail.com>
2019-05-09 00:29:50 -0400

    Done commenting

--- transvenous_pacing_gui/server.py
+++ transvenous_pacing_gui/server.py
@@ -1,32 +1,72 @@
+"""
+.. module:: server
+    :platform: Windows
+    :synopsis: Multithreaeded TCP/IP socket server to avoid hanging the GUI
+ 
+.. moduleauthor:: Nam Tran <tranngocnam97@gmail.com>
+"""
+
+# Standard Library imports
 import socket
 import threading
-
 from queue import Queue
 
 class Server:
+    """Multithreaeded TCP/IP socket server to avoid hanging the GUI
+ 
+    This class provides basic functionality to connect to its partner TCP/IP
+    socket client.
+    """
 
     def __init__(self, port, host=socket.gethostname()):
+        """Constructor
+ 
+        Args:
+            port (int): Network port for the socket connection.
+            host (str, optional): Hostname for the server to use. Defaults to localhost.
+        """
         self.host = host
         self.port = port
 
     def start(self, queue):
+        """Starts a socket server listener in a new thread.
+
+        Args:
+            queue (queue): Mutex for the server thread
+ 
+        Returns:
+            Server thread handle.
+        """
         print("Starting Server Socket")
+
+        # Initialises the socket server
         self.server = socket.socket()
         self.server.bind((self.host, self.port))
         self.server.listen(5)
         
+        # Sets up and starts the server thread
         self.server_thread = threading.Thread(target=self.listen, args=(queue,))
         self.server_thread.start()
 
+        # Returns the server thread
         return self.server_thread
 
     def stop(self):
+        """Stops the client-server connection. It also closes the open threads.
+        """
         print("Closing connection with client...")
         self.stop_accept()
         self.server_thread.join()
         self.connection.close()
 
     def receive_data(self, connection, address, queue):
+        """Blocking wait for a message
+
+        Args:
+            connection: socket connection to read from
+            address: localhost address
+            queue: mutex to output read message to
+        """
         message = connection.recv(1024)
 
         queue.put(message)
@@ -34,45 +74,77 @@ class Server:
         return message
 
     def set_hostname(self, host):
+        """Sets the target hostname
+ 
+        Args:
+            host (str): Hostname to connect to.
+        """
         self.host = host
 
     def get_hostname(self):
+        """Gets the target hostname
+ 
+        Returns:
+            hostname of the target server
+        """
         return self.host
 
     def listen(self, queue):
+        """Creates an active listener loop for incoming data
+
+        Args:
+            queue: mutex to output read message to
+        """
         (self.connection, self.address) = self.server.accept()
         print("Connection established with client...")
 
+        # Infinite loop that needs to be broken with a close command
         while True:
+            # Receive data from the receive data thread
             message = self.receive_data(self.connection, self.address, queue)
 
             if message == b'close':
                 break
 
     def stop_accept(self):
+        """Opens a new socket client to force close a socket server if it is hanging."""
+
+        # Try to open the tempory client and close the socket
         try:
             with socket.socket() as tmp_client:
+                # Connect to the server
                 tmp_client.connect((self.host, self.port))
                 stop_message = "close"
+                # Send the close command
                 tmp_client.send(stop_message.encode())
                 print("Forcing server closed")
+        # Print any exceptions
         except Exception as e:
             print(e)
 
+# Code to run if the script is called by itself
 if __name__ == '__main__':
+    # Mutex
     queue = Queue()
 
+    # Server instantiation
     test_server = Server(port=911)
     test_server.start(queue)
 
+    # Message to be
     message = ''
 
+    # Infinite loop to be broken out of
     while True:
+        # If the queue is not empty
         if not queue.empty():
+            # Get value from the queue
             message = queue.get()
             print(message)
 
+            # Break out of the loop if the close command is found
             if message == b'close':
                 break
 
+    # Close out of all server connection stuff
     test_server.stop()
--- transvenous_pacing_gui/signals.py
+++ transvenous_pacing_gui/signals.py
@@ -1,3 +1,11 @@
+"""
+.. module:: client
+    :platform: Windows
+    :synopsis: ECG signal lookup class with signal manipulation
+ 
+.. moduleauthor:: Nam Tran, Brianna Cathey
+"""
+
 # Standard Library imports
 import json
 import os
@@ -6,10 +14,11 @@ import sys
 class Signals():
     """ECG signal lookup class with signal manipulation
  
-    This class provides basic functionality to connect to its partner TCP/IP
-    socket server.
+    This class reads in a JSON file as a dictionary and uses that for its base values.
+    It then creates the an updated signal to account for heart rate.
     """
 
+    # Position index to key
     signal_index = {
         0: 'RIP',
         1: 'SVC',
@@ -24,33 +33,64 @@ class Signals():
     }
 
     def __init__(self):
+        """Constructor"""
+
+        # Gets the JSON file path
         json_file = os.path.abspath('{}/../signals.json'.format(__file__))
 
+        # Opens the JSON file
         with open(json_file) as f:
+            # Creates the dictionary for the JSON file
             self.ecg_signals = json.load(f)
 
     def get_signal(self, location, rate, version=0):
+        """Gets the ECG signal at a specified position with a specified rate
+ 
+        Args:
+            location (str): Heart position to display an ECG signal for
+            rate (int): Heartrate to edit the signal to
+            version (int): Either 0 or 1
+        """
+
+        # Get the signal dictionary entry
         signal = self.ecg_signals[location][version]
 
+        # Break it up into an x and a y as a copy to avoid overwriting the base signals
         x = signal['x'].copy()
         y = signal['y'].copy()
 
+        # If the position is not unknown
         if not location == 'RIP':
+            # Match start and finish y values
             y[-1] = y[0]
+
+            # Get the total duration of 1 heart beat
             time_beat = x[-1] - x[0]
+            # Calculate the desired R-R interval
             r = 1 / (rate/60)
 
+            # If the R-R interval is greater than the single heart beat
             if r > time_beat:
+                # Append a new x value that is the start of the next beat
                 x.append(x[0] + r)
+                # Shift all x values to be at 0
                 x = [xx - x[0] for xx in x]
+                # Add in a y value to match the first one at the end
                 y.append(y[-1])
+            # If the R-R interval is less than or equal to a single heart beat
             else:
+                # Get the length of 1 heart beat
                 length = x[-1] - x[0]
+                # Get a proportion to shrink the beats by to fit them
                 d = r / length
+                # Add in the last value to be just at the end
                 x.append(x[0])
+                # Compress the values so they fit with the desired heart rate
                 x = [xx * d for xx in x]
+                # Shift all x values to be at 0
                 x = [xx - x[0] for xx in x]
 
+        # Return the massaged values
         return x, y
 
 # Code to run if the script is called by itself - for debugging
